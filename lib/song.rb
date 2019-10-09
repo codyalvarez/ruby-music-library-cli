72b6b64d84fd5d13f279ac30992808c764f644b2
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-10-09 15:18:27 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 require"pry"
 class Song
-    attr_accessor :name
+    attr_accessor :name, :artist
 
     @@all = []
     
-    def initialize(name)
+    def initialize(name, artist = nil)
         @name = name
+        @artist = artist
     end
 
     def self.all
@@ -26,5 +27,12 @@ class Song
        @song.save
        @song
     end
+
+    def artist=(artist)
+        # binding.pry
+        artist.add_song(self)
+    end
+
+
 
 end
